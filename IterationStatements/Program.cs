5c19ed27a2007f161c78fa67dce099dbe806bbf6
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-05-01 21:46:41 -0500

    Started on excerise 2, need to ask questions about it

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -7,37 +7,49 @@ namespace IterationStatements
     {
         static void Main(string[] args)
         {
-                //TODO - Read each comment and complete its instruction
-                // like the example below
+            //TODO - Read each comment and complete its instruction
+            // like the example below
 
-                //Create a List called "numbers" - DONE!
-                var numbers = new List<int>();
-            
+            //Create a List called "numbers" - DONE!
+            var numbers = new List<int>();
+
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
 
-
+            var num = 0;
 
             // Create a do-while loop and use the template below:
 
             {
-                // Increment num by 1
+                do
+                {
+                    num++;
+                    numbers.Add(num);
 
-                // Then add num to the collection - numbers
-                    // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                } while (num < 10);
 
-            } // <---- While your variable is less than 100
+                // Increment num by 1 -DONE
+                // Then add num to the collection - numbers-DONE
+                // Hint: reference num inside of the Add method's parentheses-DONE
+
+
+            } // <---- While your variable is less than 100- DONE
 
 
 
             // Create a while loop
             // <--- While num is less than 200
             {
-                // Increment num by 1
-                // Then add num to the collection numbers
-                    //HINT: copy how this was done in the do while loop
+                while(num <200)
+                {
+
+                    num++;
+                    numbers.Add(num);
+                }
+                // Increment num by 1-DONE
+                // Then add num to the collection numbers-DONE
+                //HINT: copy how this was done in the do while loop-DONE
 
             }
 
@@ -47,23 +59,31 @@ namespace IterationStatements
 
 
             // Create a foreach loop using the collection - numbers
-                //In the scope of the foreach loop, print each number in numbers
-             
+            //In the scope of the foreach loop, print each number in numbers
+
+            foreach(var i in numbers)
+            {
+                Console.WriteLine($"{i}");
+
+            }
 
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
 
 
-            // Create a for loop - this will print the numbers in reverse order - from 200 to 1
-                // in your initializer set the value of i to 199
-                // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)
-                // AND as long as i is greater than or equal to 0
-                // Decrement i by 1
+            // Create a for loop - this will print the numbers in reverse order - from 200 to 1-DONE
+            // in your initializer set the value of i to 199-DONE
+            // in your conditional, as long as i is less than or equal to the amount of items in "numbers" - use (numbers.Count)-DONE
+            // AND as long as i is greater than or equal to 0-DONE
+            // Decrement i by 1-DONE
 
             //start for loop here
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
-                // place numbers[i] inside of the Console.WriteLine() method
+
+                Console.WriteLine($"{numbers[i]}");
+                // place numbers[i] inside of the Console.WriteLine() method-DONE
             }
 
             //------------End of exercise
